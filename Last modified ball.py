4a53bf773db10cfa4f7284919e29fa598822878c
Shibam Nath <112779826+shibam120302@users.noreply.github.com>
2023-01-09 08:39:24 +0530

    Create Last modified ball.py

---
+++ Last modified ball.py
@@ -0,0 +1,9 @@
+class Solution():
+    def solve(self, N, A):
+        #your code here
+        c = 1
+        for i in range(n-1,-1,-1):
+            if A[i] != 9:
+                break
+        return i+1
+        
